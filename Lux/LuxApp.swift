1b1c7d7c7a4eccd4923c49aef321805f928e9450
c22dev <102235607+c22dev@users.noreply.github.com>
2025-03-29 23:30:19 +0100

    Initial Commit

---
+++ Lux/LuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuxApp.swift
+//  Lux
+//
+//  Created by Constantin Clerc on 29.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct LuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
