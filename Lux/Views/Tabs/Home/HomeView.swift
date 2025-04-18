97f921e2ef0788f1ec99ca2e3f90c10dc63d27e8
c22dev <102235607+c22dev@users.noreply.github.com>
2025-04-18 16:23:44 +0200

    Basic layout for HomeView

---
+++ Lux/Views/Tabs/Home/HomeView.swift
@@ -0,0 +1,56 @@
+//
+//  HomeView.swift
+//  Lux
+//
+//  Created by Constantin Clerc on 18.04.2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemBackground).ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                ZStack(alignment: .top) {
+                    Rectangle()
+                        .fill(Color(.secondarySystemBackground))
+                        .frame(maxHeight: .infinity)
+                        .frame(height: 200)
+                        .clipShape(
+                            .rect(
+                                topLeadingRadius: 0,
+                                bottomLeadingRadius: 40,
+                                bottomTrailingRadius: 40,
+                                topTrailingRadius: 0,
+                                style: .continuous
+                            )
+                        )
+                }
+                .ignoresSafeArea(edges: .top)
+                
+                ZStack {
+                    Rectangle()
+                        .fill(Color(.secondarySystemBackground))
+                        .frame(maxHeight: .infinity)
+                        .clipShape(
+                            .rect(
+                                topLeadingRadius: 38,
+                                bottomLeadingRadius: 0,
+                                bottomTrailingRadius: 0,
+                                topTrailingRadius: 38,
+                                style: .continuous
+                            )
+                        )
+                }
+                .ignoresSafeArea(edges: .bottom)
+            }
+        }
+    }
+}
+
+
+#Preview {
+    HomeView()
+}
