02dec6c39ab0d6c69ca0b70dd7762fc048c9a4d5
c22dev <102235607+c22dev@users.noreply.github.com>
2025-04-18 15:31:18 +0200

    Added a basic LocationManager

---
+++ Lux/Classes/LocationManager.swift
@@ -0,0 +1,83 @@
+//
+//  LocationManager.swift
+//  Lux
+//
+//  Created by Constantin Clerc on 18.04.2025.
+//
+//  https://needone.app/get-location-coordinates-using-cllocationmanager-in-swift/
+
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
+    @Published var location: CLLocation?
+    @Published var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    @Published var permissionDenied = false
+    @Published var errorMessage: String?
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        self.authorizationStatus = locationManager.authorizationStatus
+        
+        if authorizationStatus == .authorizedWhenInUse || authorizationStatus == .authorizedAlways {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        DispatchQueue.main.async {
+            self.location = locations.last
+            self.errorMessage = nil
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        DispatchQueue.main.async {
+            self.authorizationStatus = status
+            
+            switch status {
+            case .authorizedWhenInUse, .authorizedAlways:
+                self.permissionDenied = false
+                self.locationManager.startUpdatingLocation()
+            case .denied, .restricted:
+                self.permissionDenied = true
+                self.errorMessage = "Location access was denied. Please enable it in Settings to use the app properly."
+            case .notDetermined:
+                self.permissionDenied = false
+                self.errorMessage = "Waiting for location permission..."
+            @unknown default:
+                self.permissionDenied = true
+                self.errorMessage = "Unknown authorization status"
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        DispatchQueue.main.async {
+            if let clError = error as? CLError {
+                switch clError.code {
+                case .denied:
+                    self.permissionDenied = true
+                    self.errorMessage = "Location access was denied. Please enable it in Settings."
+                case .locationUnknown:
+                    self.errorMessage = "Unable to determine your location. Please try again later."
+                default:
+                    self.errorMessage = "Error retrieving location: \(error.localizedDescription)"
+                }
+            } else {
+                self.errorMessage = "Error retrieving location: \(error.localizedDescription)"
+            }
+        }
+    }
+    
+    func requestLocationPermission() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func stopLocationUpdates() {
+        locationManager.stopUpdatingLocation()
+    }
+}
