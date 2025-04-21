f87a77d7961b6c1b61a28badc57c62a1439054f0
c22dev <102235607+c22dev@users.noreply.github.com>
2025-04-21 23:40:59 +0200

    simplify IndividualStopView

--- Lux/Views/Tabs/Stops/IndividualStop/IndividualStopView.swift
+++ Lux/Views/Tabs/Stops/IndividualStop/IndividualStopView.swift
@@ -12,31 +12,19 @@ struct IndividualStopView: View {
     @State var stop: SearchResult
     
     var body: some View {
-        ZStack {
-            Color(.systemBackground)
+        ZStack(alignment: .topLeading) {
+            Color(.secondarySystemBackground).opacity(0.8)
                 .ignoresSafeArea()
-                .opacity(0.9)
-            
-            VStack(spacing: 0) {
-                VStack(spacing: 0) {
-                    StopHeaderView(stop: stop)
-                        .padding(.top, 95)
-                        .padding(.horizontal, 20)
-                        .padding(.bottom, 20)
-                    Divider()
-                    VStack(alignment: .center) {
-                        StopView(stop: stop, maxGroupsToShow: 15, fromStops: true)
-                            .padding(.top, -5)
-                    }
+            VStack(alignment: .leading, spacing: 0) {
+                StopHeaderView(stop: stop)
+                    .padding(.top, 95)
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 20)
+                Divider()
+                VStack(alignment: .center) {
+                    StopView(stop: stop, maxGroupsToShow: 50, fromStops: true)
+                        .padding(.top, -5)
                 }
-                .background(
-                    RoundedRectangle(cornerRadius: 40, style: .continuous)
-                        .fill(Color(.secondarySystemBackground).opacity(0.8))
-                        .edgesIgnoringSafeArea([.bottom, .horizontal])
-                )
-                .clipShape(
-                    ContainerRelativeShape()
-                )
             }
             .edgesIgnoringSafeArea(.vertical)
         }
