c426bae3bc9929d77436479cd1a067a783f97114
c22dev <102235607+c22dev@users.noreply.github.com>
2025-04-24 17:09:21 +0200

    added the ability to view itinerary from expanded

---
+++ Lux/Views/Stop/Expanded/Components/Content/Route/ExpandedGroupView.swift
@@ -0,0 +1,42 @@
+//
+//  ExpandedGroupView.swift
+//  Lux
+//
+//  Created by Constantin Clerc on 22.04.2025.
+//
+
+import SwiftUI
+import LuxCom
+
+struct ExpandedGroupView: View {
+    @Environment(\.colorScheme) var colorScheme
+    let group: GroupedStopTime
+    @Binding var animateIn: Bool
+    var animation: Namespace.ID
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            NavigationLink(destination: {
+                if let tripId = group.stopTimes.first?.tripId {
+                    ItineraryView(tripId: tripId)
+                }
+            }) {
+                HStack {
+                    LinePill(line: group.routeShortName, mode: group.stopTimes.first?.mode ?? .bus)
+                        .matchedGeometryEffect(id: "pill_\(group.id)", in: animation)
+                    Text(group.headsign)
+                        .font(.headline)
+                        .lineLimit(1)
+                        .foregroundStyle(colorScheme == .dark ? Color.white: Color.black)
+                    Spacer()
+                }
+            }
+            
+            VStack(alignment: .leading, spacing: 8) {
+                ForEach(Array(group.stopTimes.prefix(4).enumerated()), id: \.element.id) { index, stopTime in
+                    DepartureTimeRow(stopTime: stopTime, animateIn: $animateIn, index: index)
+                }
+            }
+        }
+    }
+}
