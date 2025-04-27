c076f3f122134719646be0a995c651a76e4c142b
c22dev <102235607+c22dev@users.noreply.github.com>
2025-04-27 18:37:33 +0200

    added the tags for stops

---
+++ Lux/Views/Trips/Details/Individual/Components/Stop/StopTypeLabel.swift
@@ -0,0 +1,38 @@
+//
+//  StopTypeLabel.swift
+//  Lux
+//
+//  Created by Constantin Clerc on 27.04.2025.
+//
+
+import SwiftUI
+
+struct StopTypeLabel: View {
+    let isDepartureStop: Bool
+    let isArrivalStop: Bool
+    let legColor: Color
+    
+    var body: some View {
+        Group {
+            if isDepartureStop {
+                Text("Départ")
+                    .font(.caption)
+                    .fontWeight(.medium)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 3)
+                    .background(Color.green.opacity(0.15))
+                    .foregroundColor(.green)
+                    .cornerRadius(8)
+            } else if isArrivalStop {
+                Text("Arrivée")
+                    .font(.caption)
+                    .fontWeight(.medium)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 3)
+                    .background(Color.red.opacity(0.15))
+                    .foregroundColor(.red)
+                    .cornerRadius(8)
+            }
+        }
+    }
+}
