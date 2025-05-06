47449f8cf2bdf216e537748e72a9564b4423e751
c22dev <102235607+c22dev@users.noreply.github.com>
2025-05-06 23:46:59 +0200

    reuse v2 apis

--- Lux/Classes/ViewModels/ItineraryViewModel.swift
+++ Lux/Classes/ViewModels/ItineraryViewModel.swift
@@ -200,7 +200,7 @@ final class ItineraryViewModel: ObservableObject {
 
     
     private func createRouteOverlay(for leg: Leg, withColor color: Color) -> RouteOverlay? {
-        let polyline = Polyline(encodedPolyline: leg.legGeometry.points, precision: 1e7)
+        let polyline = Polyline(encodedPolyline: leg.legGeometry.points, precision: 1e6)
         
         guard let coordinates = polyline.coordinates, !coordinates.isEmpty else { return nil }
         
