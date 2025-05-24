474312449b6544fd93b871b260c631f1bf6389a6
c22dev <102235607+c22dev@users.noreply.github.com>
2025-05-24 17:41:52 +0200

    test, will implem later

--- Lux/Views/Itinerary/ItineraryView.swift
+++ Lux/Views/Itinerary/ItineraryView.swift
@@ -137,6 +137,21 @@ struct ItineraryView: View {
     }
 }
 
+//extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+//    override open func viewDidLoad() {
+//        super.viewDidLoad()
+//        interactivePopGestureRecognizer?.delegate = self
+//    }
+//    
+//    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+//        return viewControllers.count > 1
+//    }
+//    
+//    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+//        true
+//    }
+//}
+
 struct RouteOverlay: Identifiable {
     let id = UUID()
     let coordinates: [CLLocationCoordinate2D]
