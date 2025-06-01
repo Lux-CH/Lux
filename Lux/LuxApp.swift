3bec7e148201ffb48f94269910d5f38329fa7a39
c22dev <102235607+c22dev@users.noreply.github.com>
2025-06-01 02:21:58 +0200

    basic onOpenUrl implem

--- Lux/LuxApp.swift
+++ Lux/LuxApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import LuxCom
 
 @main
 struct LuxApp: App {
@@ -14,6 +15,8 @@ struct LuxApp: App {
     @StateObject private var shortcutManager = ShortcutManager()
     @StateObject private var disruptionManager = DisruptionManager()
     @StateObject private var accentColorManager = AccentColorManager.shared
+    @State private var showItinerarySheet: Bool = false
+    @State private var sharedItinerary: Itinerary?
     @ObservedObject var settings = Settings.shared
     
     var body: some Scene {
@@ -25,6 +28,39 @@ struct LuxApp: App {
                 .tint(accentColorManager.selectedAccentColor)
                 // i am fully aware this will deprecated in the future; however not putting it doesn't apply the accent everywhere; same if you only leave accentColor
                 .accentColor(accentColorManager.selectedAccentColor)
+                .onOpenURL { url in
+                    if url.pathExtension == "luxtrip" {
+                        handleItinerary(url)
+                    }
+                }
+                .fullScreenCover(isPresented: $showItinerarySheet) {
+                    if let itinerary = sharedItinerary {
+                        ItineraryView(itinerary: itinerary, fromNearby: false)
+                    }
+                }
+        }
+    }
+    func handleItinerary(_ url: URL) {
+        guard url.startAccessingSecurityScopedResource() else {
+            print("failed to acces ssr!")
+            return
+        }
+        
+        defer {
+            url.stopAccessingSecurityScopedResource()
+        }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decodedItinerary = try ItinerarySharer().decode(data)
+            
+            DispatchQueue.main.async {
+                self.sharedItinerary = decodedItinerary
+                self.showItinerarySheet = true
+            }
+            
+        } catch {
+            print(error)
         }
     }
 }
