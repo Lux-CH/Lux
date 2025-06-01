242736403b3aedf3bfc63889137f6bf3a0052abe
c22dev <102235607+c22dev@users.noreply.github.com>
2025-06-01 18:58:59 +0200

    added a verif (user prompt+size)

--- Lux/LuxApp.swift
+++ Lux/LuxApp.swift
@@ -16,6 +16,8 @@ struct LuxApp: App {
     @StateObject private var disruptionManager = DisruptionManager()
     @StateObject private var accentColorManager = AccentColorManager.shared
     @State private var showItinerarySheet: Bool = false
+    @State private var showConfirmation: Bool = false
+    @State private var inputedURL: URL?
     @State private var sharedItinerary: Itinerary?
     @ObservedObject var settings = Settings.shared
     
@@ -29,14 +31,27 @@ struct LuxApp: App {
                 // i am fully aware this will deprecated in the future; however not putting it doesn't apply the accent everywhere; same if you only leave accentColor
                 .accentColor(accentColorManager.selectedAccentColor)
                 .onOpenURL { url in
+                    inputedURL = url
                     if url.pathExtension == "luxtrip" {
-                        handleItinerary(url)
+                        showConfirmation.toggle()
                     }
                 }
                 .fullScreenCover(isPresented: $showItinerarySheet) {
                     if let itinerary = sharedItinerary {
                         ItineraryView(itinerary: itinerary, fromNearby: false)
                     }
+                }
+                .alert(isPresented: $showConfirmation) {
+                    Alert(
+                        title: Text("Êtes-vous sûr de vouloir ouvrir cet itinéraire ?"),
+                        message: Text("Cet itinéraire vous a été partagé. Assurez-vous qu’il provient d’une source fiable."),
+                        primaryButton: .default(Text("Ouvrir").bold()) {
+                            if let url = inputedURL {
+                                handleItinerary(url)
+                            }
+                        },
+                        secondaryButton: .cancel(Text("Annuler"))
+                    )
                 }
         }
     }
@@ -50,6 +65,10 @@ struct LuxApp: App {
         }
         
         do {
+            if let fileAttributes = try? FileManager.default.attributesOfItem(atPath: url.path), let size = fileAttributes[.size] as? Int64, size > 51200 {
+                print("invalid file!")
+                return
+            }
             let data = try Data(contentsOf: url)
             let decodedItinerary = try ItinerarySharer().decode(data)
             
