780a9faa07257bc62fe8e9aa7987d1a852ab1433
c22dev <102235607+c22dev@users.noreply.github.com>
2025-06-10 22:07:10 +0200

    made usershortcut more polyvalent

--- Lux/Models/UserShortcut.swift
+++ Lux/Models/UserShortcut.swift
@@ -157,20 +157,22 @@ struct UserShortcut: Identifiable, Codable, Equatable {
     }
 
     private func calculateDistancePenalty(distance: Double) -> Double {
-        if distance < 25 {
-            return 50.0
-        } else if distance < 50 {
-            return 35.0 * exp(-distance / 25.0) + 15.0
-        } else if distance < 100 {
-            return 25.0 * exp(-distance / 35.0) + 10.0
-        } else if distance < 200 {
-            let normalizedDistance = (distance - 100) / 100
-            return 8.0 * (1.0 - normalizedDistance)
-        } else if distance < 500 {
-            let normalizedDistance = (distance - 200) / 300
-            return 3.0 * (1.0 - normalizedDistance)
+        let distanceKm = distance / 1000.0
+        
+        if distanceKm < 0.025 {
+            return 100.0
+        } else if distanceKm < 0.1 {
+            return 80.0 + (20.0 * (0.1 - distanceKm) / 0.075)
+        } else if distanceKm < 0.5 {
+            return 60.0 * exp(-distanceKm * 3.0) + 20.0
+        } else if distanceKm < 1.0 {
+            let normalizedDistance = (distanceKm - 0.5) / 0.5
+            return 40.0 * (1.0 - pow(normalizedDistance, 0.7)) + 15.0
+        } else if distanceKm < 2.0 {
+            let normalizedDistance = (distanceKm - 1.0) / 1.0
+            return 25.0 * (1.0 - pow(normalizedDistance, 0.6)) + 8.0
         } else {
-            return 0.0
+            return max(0.5, 5.0 * exp(-distanceKm / 20.0))
         }
     }
 }
