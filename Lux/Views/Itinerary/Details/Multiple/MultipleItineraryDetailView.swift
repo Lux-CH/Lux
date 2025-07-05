52d67379a766a38bf46e31d15d4517f619723166
c22dev <102235607+c22dev@users.noreply.github.com>
2025-07-06 01:42:00 +0200

    improve share bttn position

--- Lux/Views/Itinerary/Details/Multiple/MultipleItineraryDetailView.swift
+++ Lux/Views/Itinerary/Details/Multiple/MultipleItineraryDetailView.swift
@@ -326,6 +326,11 @@ struct MultipleItineraryDetailView: View {
                     }
                 }
                 .padding(.bottom, 20)
+                Divider()
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 10)
+                    .frame(minHeight: 1)
+
                 let itineraarySharer = ItinerarySharer()
                 ShareButtonView(itinerary: itinerary, itineraarySharer: itineraarySharer)
             }
