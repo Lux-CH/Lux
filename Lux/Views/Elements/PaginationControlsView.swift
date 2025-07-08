90e92d61ff9178a93b01269f0301c415555845e4
Constantin Clerc <constclerc@gmail.com>
2025-07-08 13:43:31 +0200

    better pagination controls UI

--- Lux/Views/Elements/PaginationControlsView.swift
+++ Lux/Views/Elements/PaginationControlsView.swift
@@ -11,6 +11,8 @@ struct PaginationControlsView: View {
     @Binding var isLoadingEarlier: Bool
     @Binding var isLoadingLater: Bool
     @Binding var isChangingContent: Bool
+    @Environment(\.colorScheme) private var colorScheme
+    
     var isLoading: Bool
     @Binding var animateIn: Bool
     var loadEarlier: () -> Void
@@ -61,11 +63,25 @@ struct PaginationControlsView: View {
         .foregroundColor(.accentColor)
         .background(
             Capsule()
-                .fill(Color(.secondarySystemBackground))
-                .shadow(color: Color.black.opacity(0.15), radius: 10, x: 0, y: 5)
+                .fill(
+                    colorScheme == .dark
+                    ? Color(.secondarySystemBackground)
+                    : Color.white
+                )
+                .shadow(
+                    color: Color.black.opacity(colorScheme == .dark ? 0.3 : 0.15),
+                    radius: 7.5,
+                    x: 0,
+                    y: 5
+                )
                 .overlay(
                     Capsule()
-                        .stroke(Color.gray.opacity(0.1), lineWidth: 1)
+                        .stroke(
+                            colorScheme == .dark
+                            ? Color.primary.opacity(0.1)
+                            : Color.gray.opacity(0.1),
+                            lineWidth: 0.75
+                        )
                 )
                 .scaleEffect(isChangingContent ? 0.98 : 1)
                 .animation(.spring(response: 0.3, dampingFraction: 0.7), value: isChangingContent)
