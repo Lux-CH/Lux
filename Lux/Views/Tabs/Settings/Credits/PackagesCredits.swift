a345e16d617c801ce2a25bae4666e12f7403eeb5
c22dev <102235607+c22dev@users.noreply.github.com>
2025-08-02 21:03:55 +0200

    remove character that might not show under some conditions

--- Lux/Views/Tabs/Settings/Credits/PackagesCredits.swift
+++ Lux/Views/Tabs/Settings/Credits/PackagesCredits.swift
@@ -11,12 +11,12 @@ struct CreditsView: View {
     private let creditItems = [
         CreditItem(name: "Constantin Clerc", credit: String(localized: "Développeur Principal"), imageURL: "https://avatars.githubusercontent.com/u/102235607?v=4", url: "https://github.com/c22dev"),
         CreditItem(name: "Philippe Weidmann", credit: String(localized: "Données des perturbations"), imageURL: "https://avatars.githubusercontent.com/u/5843044?v=4", url: "https://apps.apple.com/fr/app/tpg-max/id1373332448"),
-        CreditItem(name: "Paul Hudson", credit: "CodeScanner — MIT", imageURL: "https://avatars.githubusercontent.com/u/190200?v=4", url: "https://github.com/twostraws/CodeScanner"),
-        CreditItem(name: "Raphaël Mor", credit: "Polyline — MIT", imageURL: "https://avatars.githubusercontent.com/u/772779?v=4", url: "https://github.com/raphaelmor/Polyline"),
-        CreditItem(name: "Yubo Qin", credit: "SymbolPicker — MIT", imageURL: "https://avatars.githubusercontent.com/u/6781789?v=4", url: "https://github.com/xnth97/SymbolPicker"),
-        CreditItem(name: "Edon Valdman", credit: "SwiftUIMessage — MIT", imageURL: "https://avatars.githubusercontent.com/u/22782929?v=4", url: "https://github.com/edonv/SwiftUIMessage"),
-        CreditItem(name: "Hirotakan", credit: "MessagePacker — MIT", imageURL: "https://avatars.githubusercontent.com/u/2901342?v=4", url: "https://github.com/hirotakan/MessagePacker"),
-        CreditItem(name: "Aether Jones", credit: "GlowGetter — MIT", imageURL: "https://avatars.githubusercontent.com/u/64797587?v=4", url: "https://github.com/Aeastr/GlowGetter")
+        CreditItem(name: "Paul Hudson", credit: "CodeScanner - MIT", imageURL: "https://avatars.githubusercontent.com/u/190200?v=4", url: "https://github.com/twostraws/CodeScanner"),
+        CreditItem(name: "Raphaël Mor", credit: "Polyline - MIT", imageURL: "https://avatars.githubusercontent.com/u/772779?v=4", url: "https://github.com/raphaelmor/Polyline"),
+        CreditItem(name: "Yubo Qin", credit: "SymbolPicker - MIT", imageURL: "https://avatars.githubusercontent.com/u/6781789?v=4", url: "https://github.com/xnth97/SymbolPicker"),
+        CreditItem(name: "Edon Valdman", credit: "SwiftUIMessage - MIT", imageURL: "https://avatars.githubusercontent.com/u/22782929?v=4", url: "https://github.com/edonv/SwiftUIMessage"),
+        CreditItem(name: "Hirotakan", credit: "MessagePacker - MIT", imageURL: "https://avatars.githubusercontent.com/u/2901342?v=4", url: "https://github.com/hirotakan/MessagePacker"),
+        CreditItem(name: "Aether Jones", credit: "GlowGetter - MIT", imageURL: "https://avatars.githubusercontent.com/u/64797587?v=4", url: "https://github.com/Aeastr/GlowGetter")
     ]
     var body: some View {
         NavigationStack {
