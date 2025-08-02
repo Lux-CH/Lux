5184f427a49ac118ce3e9ffdecf78e810c498f1c
c22dev <102235607+c22dev@users.noreply.github.com>
2025-08-02 21:01:54 +0200

    update credits

--- Lux/Views/Tabs/Settings/Credits/PackagesCredits.swift
+++ Lux/Views/Tabs/Settings/Credits/PackagesCredits.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct CreditsView: View {
     private let creditItems = [
-        CreditItem(name: "Constantin Clerc", credit: "Développeur Principal", imageURL: "https://avatars.githubusercontent.com/u/102235607?v=4", url: "https://github.com/c22dev"),
-        CreditItem(name: "Philippe Weidmann", credit: "Données des perturbations", imageURL: "https://avatars.githubusercontent.com/u/5843044?v=4", url: "https://apps.apple.com/fr/app/tpg-max/id1373332448"),
+        CreditItem(name: "Constantin Clerc", credit: String(localized: "Développeur Principal"), imageURL: "https://avatars.githubusercontent.com/u/102235607?v=4", url: "https://github.com/c22dev"),
+        CreditItem(name: "Philippe Weidmann", credit: String(localized: "Données des perturbations"), imageURL: "https://avatars.githubusercontent.com/u/5843044?v=4", url: "https://apps.apple.com/fr/app/tpg-max/id1373332448"),
         CreditItem(name: "Paul Hudson", credit: "CodeScanner — MIT", imageURL: "https://avatars.githubusercontent.com/u/190200?v=4", url: "https://github.com/twostraws/CodeScanner"),
         CreditItem(name: "Raphaël Mor", credit: "Polyline — MIT", imageURL: "https://avatars.githubusercontent.com/u/772779?v=4", url: "https://github.com/raphaelmor/Polyline"),
         CreditItem(name: "Yubo Qin", credit: "SymbolPicker — MIT", imageURL: "https://avatars.githubusercontent.com/u/6781789?v=4", url: "https://github.com/xnth97/SymbolPicker"),
@@ -59,8 +59,8 @@ struct CreditsView: View {
                 SectionHeader(
                     icon: "person.3.fill",
                     iconColor: .blue,
-                    title: "Crédits",
-                    subtitle: "Ci-dessous une liste des crédits de l'application, notamment des différents autres modules utilisés par Lux."
+                    title: String(localized: "Crédits"),
+                    subtitle: String(localized: "Ci-dessous une liste des crédits de l'application, notamment des différents autres modules utilisés par Lux.")
                 )
             }
         }
