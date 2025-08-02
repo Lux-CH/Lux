ada23f0c40def9d93953869f6a299864982120fe
Constantin Clerc <constclerc@gmail.com>
2025-08-03 01:52:53 +0200

    added a cleanup for caches every 15 launch

---
+++ Lux/Classes/CacheCleaner.swift
@@ -0,0 +1,53 @@
+//
+//  CacheCleaner.swift
+//  Lux
+//
+//  Created by Constantin Clerc on 03.08.2025.
+//
+
+import Foundation
+
+struct CacheCleaner {
+    static func performCleanup() async {
+        guard let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first,
+              let libraryPath = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).first else {
+            return
+        }
+                
+        cleanDirectory(at: URL(fileURLWithPath: NSTemporaryDirectory()))
+        removeDirectory(at: documentsPath.appendingPathComponent("Inbox"))
+        cleanDirectory(at: libraryPath.appendingPathComponent("SplashBoard"))
+    }
+    
+    static func cleanDirectory(at url: URL) {
+        let fm = FileManager.default
+        
+        guard fm.fileExists(atPath: url.path) else {
+            return
+        }
+        
+        do {
+            let contents = try fm.contentsOfDirectory(at: url, includingPropertiesForKeys: nil)
+            
+            for item in contents {
+                try fm.removeItem(at: item)
+            }
+        } catch {
+            print(error)
+        }
+    }
+
+    static func removeDirectory(at url: URL) {
+        let fm = FileManager.default
+        
+        guard fm.fileExists(atPath: url.path) else {
+            return
+        }
+        
+        do {
+            try fm.removeItem(at: url)
+        } catch {
+            print(error)
+        }
+    }
+}
