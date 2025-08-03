0f72a0513ba38f3fa19fc927fd2e7aa576f4cee7
Constantin Clerc <constclerc@gmail.com>
2025-08-03 19:27:14 +0200

    added refresh button on widget

--- StopWidget/StopWidgetBundle.swift
+++ StopWidget/StopWidgetBundle.swift
@@ -7,10 +7,22 @@
 
 import WidgetKit
 import SwiftUI
+import AppIntents
 
 @main
 struct StopWidgetBundle: WidgetBundle {
     var body: some Widget {
         StopWidget()
+    }
+}
+
+
+struct RefreshWidgetIntent: AppIntent {
+    static var title: LocalizedStringResource = "Actualiser les départs"
+    
+    @MainActor
+    func perform() async throws -> some IntentResult {
+        WidgetCenter.shared.reloadAllTimelines()
+        return .result()
     }
 }
