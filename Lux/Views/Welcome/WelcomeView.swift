ef432a49c1c92a5b1f29d73235d374a3c2eb750a
Constantin Clerc <constclerc@gmail.com>
2025-08-06 01:42:46 +0200

    dont show last divider on compact if requested

--- Lux/Views/Welcome/WelcomeView.swift
+++ Lux/Views/Welcome/WelcomeView.swift
@@ -294,7 +294,7 @@ struct WelcomeView: View {
             level: 0.0,
             areas: [],
             score: 0.0
-        ), maxGroupsToShow: 3, fromStops: false)
+        ), maxGroupsToShow: 3, fromStops: false, dontShowLastDivider: true)
         .padding(.bottom, 7.5)
         .background(Color(.systemBackground).clipShape(
             UnevenRoundedRectangle(
