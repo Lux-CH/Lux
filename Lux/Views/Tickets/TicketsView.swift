c9ab125dd97a64b8827387c3704dd0b68aa10d7b
Constantin Clerc <constclerc@gmail.com>
2025-08-09 23:01:08 +0200

    add clear disclaimers for tickets

--- Lux/Views/Tickets/TicketsView.swift
+++ Lux/Views/Tickets/TicketsView.swift
@@ -102,7 +102,13 @@ struct UserTypeSelector: View {
                 }
                 .pickerStyle(.segmented)
                 .padding(.horizontal, 20)
-                .padding(.bottom, 20)
+                
+                Text("Note : Les tickets ci-dessous seront achetés via un SMS envoyé par le service officiel des TPG suite à votre confirmation. Des frais SMS standard peuvent s’appliquer.\nLes tickets ne sont pas gérés par Lux et leurs tarifs peuvent évoluer à tout moment sans préavis.")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 20)
+                
             }
         }
     }
