3d52007fcbe8629b4e230a361b8c27277d9176ac
Constantin Clerc <constclerc@gmail.com>
2025-08-12 22:02:14 +0200

    added new accent colors + icon
    summer 2025 styles, more pastel-ish

--- Lux/Classes/AccentColorManager.swift
+++ Lux/Classes/AccentColorManager.swift
@@ -13,15 +13,19 @@ class AccentColorManager: ObservableObject {
     @AppStorage("selectedAccentColor") private var selectedColorName: String = String(localized: "Orange Lux")
     
     let availableColors: [(name: String, color: Color, iconName: String?)] = [
-        (String(localized: "Orange Lux"), .orange, nil),
-        (String(localized: "Bleu SBB CFF"), Color(hex: "2d327d"), "SBB-Blue"),
         (String(localized: "Rouge SBB CFF"), Color(hex: "EB0000"), "SBB-Red"),
-        (String(localized: "Orange TPG"), Color(hex: "FC5412"), "tpg"),
-        (String(localized: "Vert CGTE"), Color(hex: "2D8859"), "cgte"),
-        (String(localized: "Jaune Mouettes"), .yellow, nil),
         (String(localized: "Rouge Grenat"), Color(hex: "85142B"), "servette"),
-        (String(localized: "Bleu Ice Tea"), Color(hex: "3182DB"), "icetea"),
         (String(localized: "Rose UNIGE"), Color(hex: "D9005D"), "unige"),
+        (String(localized: "Orange TPG"), Color(hex: "FC5412"), "tpg"),
+        (String(localized: "Orange Lux"), .orange, nil),
+        (String(localized: "Jaune Mouettes"), .yellow, nil),
+        (String(localized: "Vert Bastions"), .green, "cgte"),
+        (String(localized: "Vert CGTE"), Color(hex: "2D8859"), "cgte"),
+        (String(localized: "Bleu Léman"), Color(hex: "2EFEDA"), "icetea"),
+        (String(localized: "Bleu Ice Tea"), Color(hex: "3182DB"), "icetea"),
+        (String(localized: "Bleu SBB CFF"), Color(hex: "2d327d"), "SBB-Blue"),
+        (String(localized: "Violet SCNF"), .indigo, "sncf"),
+
     ]
     
     var selectedAccentColor: Color {
