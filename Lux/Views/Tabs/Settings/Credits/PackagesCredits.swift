96bc95c42fac92892e11921cec86ddaa947d86ac
Constantin Clerc <constclerc@gmail.com>
2025-08-19 02:39:02 +0200

    remove previously used package (SwiftUIMessage)

--- Lux/Views/Tabs/Settings/Credits/PackagesCredits.swift
+++ Lux/Views/Tabs/Settings/Credits/PackagesCredits.swift
@@ -15,7 +15,6 @@ struct CreditsView: View {
         CreditItem(name: "Paul Hudson", credit: "CodeScanner - MIT", imageURL: "https://avatars.githubusercontent.com/u/190200?v=4", url: "https://github.com/twostraws/CodeScanner"),
         CreditItem(name: "Raphaël Mor", credit: "Polyline - MIT", imageURL: "https://avatars.githubusercontent.com/u/772779?v=4", url: "https://github.com/raphaelmor/Polyline"),
         CreditItem(name: "Yubo Qin", credit: "SymbolPicker - MIT", imageURL: "https://avatars.githubusercontent.com/u/6781789?v=4", url: "https://github.com/xnth97/SymbolPicker"),
-        CreditItem(name: "Edon Valdman", credit: "SwiftUIMessage - MIT", imageURL: "https://avatars.githubusercontent.com/u/22782929?v=4", url: "https://github.com/edonv/SwiftUIMessage"),
         CreditItem(name: "Hirotakan", credit: "MessagePacker - MIT", imageURL: "https://avatars.githubusercontent.com/u/2901342?v=4", url: "https://github.com/hirotakan/MessagePacker"),
         CreditItem(name: "Aether Jones", credit: "GlowGetter - MIT", imageURL: "https://avatars.githubusercontent.com/u/64797587?v=4", url: "https://github.com/Aeastr/GlowGetter")
     ]
