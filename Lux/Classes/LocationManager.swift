5ea868ed54b495363bef34d4cc743a84e137d960
c22dev <102235607+c22dev@users.noreply.github.com>
2025-08-21 17:58:40 +0200

    added a 25m distanceFilter on locman

--- Lux/Classes/LocationManager.swift
+++ Lux/Classes/LocationManager.swift
@@ -19,6 +19,7 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     override init() {
         super.init()
         locationManager.delegate = self
+        locationManager.distanceFilter = 25.0
         locationManager.requestWhenInUseAuthorization()
         
         self.authorizationStatus = locationManager.authorizationStatus
