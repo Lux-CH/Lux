4e9cbba73e74e07cbd29dc8f91a4a6f6da227d6a
c22dev <102235607+c22dev@users.noreply.github.com>
2026-04-05 01:54:13 +0200

    remove useless debug stuff

--- Lux/Classes/ViewModels/StopsViewModel.swift
+++ Lux/Classes/ViewModels/StopsViewModel.swift
@@ -139,10 +139,6 @@ class StopsViewModel: ObservableObject {
                 
                 await MainActor.run {
                     self.searchResults = filteredResults
-                    for result in filteredResults {
-                        print(result.id)
-                        print(result.name)
-                    }
                     self.lastFetchedLocation = fetchLocation
                     self.isLoading = false
                 }
