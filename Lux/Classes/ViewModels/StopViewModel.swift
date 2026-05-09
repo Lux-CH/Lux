163c26d944af4ec2be5800552b0aa065da527316
c22dev <102235607+c22dev@users.noreply.github.com>
2026-05-09 15:14:36 +0200

    Revert "MAYBE fix refresh changes ? not sure"
    This reverts commit 5c8103e468a1c9765560c5acef2c2b0c45b380d7.

--- Lux/Classes/ViewModels/StopViewModel.swift
+++ Lux/Classes/ViewModels/StopViewModel.swift
@@ -86,7 +86,7 @@ class StopViewModel: ObservableObject {
     
     @MainActor
     func refreshDepartures(showLoading: Bool) async {
-        await refreshDepartures(forTime: getReferenceTime(), showLoading: showLoading)
+        await refreshDepartures(forTime: currentTime, showLoading: showLoading)
     }
     
     @MainActor
@@ -136,7 +136,6 @@ class StopViewModel: ObservableObject {
             }
         }
         await backgroundRefreshTask?.value
-        backgroundRefreshTask = nil
     }
     
     private func fetchDeparturesAndArrivals(for time: Date) async throws -> (departures: StopTimes, arrivals: StopTimes) {
@@ -169,14 +168,11 @@ class StopViewModel: ObservableObject {
     }
 
     private func refreshDeparturesInBackground() async {
-        guard backgroundRefreshTask == nil || backgroundRefreshTask?.isCancelled == true else {
-            return
-        }
-        let refreshTime = getReferenceTime()
+        backgroundRefreshTask?.cancel()
         
         backgroundRefreshTask = Task {
             do {
-                let (departuresData, arrivalsData) = try await fetchDeparturesAndArrivals(for: refreshTime)
+                let (departuresData, arrivalsData) = try await fetchDeparturesAndArrivals(for: currentTime)
                 if Task.isCancelled {
                     backgroundRefreshTask = nil
                     return
