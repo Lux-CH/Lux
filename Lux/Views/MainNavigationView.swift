820662dfbf7661c153a90491cf380a890ab4e247
c22dev <102235607+c22dev@users.noreply.github.com>
2026-06-13 03:56:53 +0200

    fix xcode 27 warnings

--- Lux/Views/MainNavigationView.swift
+++ Lux/Views/MainNavigationView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 import LuxCom
 
 enum ViewMode: CaseIterable {
--- Lux/Views/Tabs/Home/Nearby/NearbyStopsView.swift
+++ Lux/Views/Tabs/Home/Nearby/NearbyStopsView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 import LuxCom
 import LuxComHAFAS
 import Network
