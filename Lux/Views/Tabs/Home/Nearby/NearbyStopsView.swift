a4ddc2441dd99503f1cd1870c2a8bbf5ae9a2662
c22dev <102235607+c22dev@users.noreply.github.com>
2026-07-25 16:27:38 +0000

    trim one route group on station cards, move train glyph next to chevron

--- Lux/Views/Tabs/Home/Nearby/NearbyStopsView.swift
+++ Lux/Views/Tabs/Home/Nearby/NearbyStopsView.swift
@@ -165,7 +165,7 @@ struct NearbyStopsView: View {
                         let nearbyStops = Array(progress.searchResults.prefix(2))
                         if let station = nearbyStops.first, station.servesRail {
                             ZStack {
-                                StopView(stop: station, maxGroupsToShow: 8, fromStops: false, isLastStopOverall: true)
+                                StopView(stop: station, maxGroupsToShow: 7, fromStops: false, isLastStopOverall: true)
                             }
                             .frame(maxWidth: .infinity)
                         } else {
